166fc3726ee76880353a67ad7b12473a0371abbe
Avogardo <jakub.i.wolny@gmail.com>
2019-04-03 18:59:47 +0200

    add form types model

---
+++ react-app/src/FormModel.js
@@ -0,0 +1,9 @@
+import FormTypes from './FormTypes';
+
+export default class FormModel {
+  constructor(type = FormTypes.text, text = 'input', action = () => { console.log('modified'); }) {
+    this.type = type;
+    this.text = text;
+    this.action = action;
+  }
+}
