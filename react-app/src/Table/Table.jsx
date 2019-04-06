8f460aaeaa2e887a9bc54b65aa15d1ab8a30ff19
Avogardo <jakub.i.wolny@gmail.com>
2019-04-06 21:11:08 +0200

    add simple table styles

--- react-app/src/Table/Table.jsx
+++ react-app/src/Table/Table.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Table.css';
 
 const Table = ({ rows }) => {
-  const getCells = cells => cells.map((cell, index) =>
-    <td key={`${index}cell`}>{cell}</td>);
+  const getCells = cells => cells.map((cell, index) => {
+    if (cell === true) {
+      return <td key={`${index}cell`}><input type="checkbox" /></td>;
+    }
+    return <td key={`${index}cell`}>{cell}</td>;
+  });
 
   const getRows = (rows) => rows.map((row, index) => <tr key={`${index}row`}>
     {getCells(row)}
