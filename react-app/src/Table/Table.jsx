19d0e6853c2a9c604889e765616fac4a165da8c3
Avogardo <jakub.i.wolny@gmail.com>
2019-04-07 21:39:13 +0200

    convert table function component to a class component

--- react-app/src/Table/Table.jsx
+++ react-app/src/Table/Table.jsx
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Table.css';
 
-const Table = ({ rows }) => {
-  const getCells = cells => cells.map((cell, index) => {
-    if (cell === true) {
-      return <td key={`${index}cell`}><input type="checkbox" /></td>;
-    } else if (typeof cell === 'string' && cell.includes('.png')) {
-      return <td key={`${index}cell`}><img src={cell} alt="js logo" /></td>;
-    }
-    return <td key={`${index}cell`}>{cell}</td>;
-  });
+class Table extends Component {
+  getCells(cells) {
+    return cells.map((cell, index) => {
+      if (cell === true) {
+        return <td key={`${index}cell`}><input type="checkbox" /></td>;
+      } else if (typeof cell === 'string' && cell.includes('.png')) {
+        return <td key={`${index}cell`}><img src={cell} alt="js logo" /></td>;
+      }
+      return <td key={`${index}cell`}>{cell}</td>;
+    });
+  }
 
-  const getRows = (rows) => rows.map((row, index) => <tr key={`${index}row`}>
-    {getCells(row)}
-  </tr>);
-
-  if (rows[0].length) {
-    return (
-      <div className="table-wrapper">
-        <table>
-          <tbody>
-            {getRows(rows)}
-          </tbody>
-        </table>
-      </div>
+  getRows(rows) {
+    return rows.map((row, index) =>
+      <tr key={`${index}row`}>
+        {this.getCells(row)}
+      </tr>
     );
   }
-  return <p>loading...</p>;
-};
+
+  render() {
+    const { rows } = this.props;
+    return (
+      rows[0].length ?
+        <div className="table-wrapper">
+          <table>
+            <tbody>
+            {this.getRows(rows)}
+            </tbody>
+          </table>
+          <button>Hide table</button>
+        </div>
+        :
+        <p>loading...</p>
+    );
+  }
+}
 
 Table.defaultProps = {
   rows: [[]],
