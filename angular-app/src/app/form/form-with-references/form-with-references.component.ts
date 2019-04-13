32f6dc45f1cd01143c3c8abaa457c4c3bfe29b6e
Avogardo <avogardo0@gmail.com>
2019-04-14 01:02:06 +0200

    add form with references functionality and styles

--- angular-app/src/app/form/form-with-references/form-with-references.component.ts
+++ angular-app/src/app/form/form-with-references/form-with-references.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
+import { FormService } from '../form.service';
+import FormTypes from '../FormTypes';
+import FormModel from '../FormModel';
 
 @Component({
   selector: 'app-form-with-references',
   templateUrl: './form-with-references.component.html',
-  styleUrls: ['./form-with-references.component.scss']
+  styleUrls: ['./form-with-references.component.scss'],
 })
-export default class FormWithReferencesComponent implements OnInit {
-  constructor() {
+export default class FormWithReferencesComponent {
+  @ViewChildren('inputElement')
+  viewChildren !: QueryList<ElementRef>;
+
+  formTypes: typeof FormTypes = FormTypes;
+  inputs: FormModel[] = [];
+
+  constructor(private formService: FormService) {
+    this.inputs = this.formService.getInputs();
   }
 
-  ngOnInit() {
+  onKeyAction(): void {
+    this.viewChildren.forEach((element: ElementRef, index: number): void => {
+      this.inputs[index].text = element.nativeElement.value;
+    });
   }
 }
