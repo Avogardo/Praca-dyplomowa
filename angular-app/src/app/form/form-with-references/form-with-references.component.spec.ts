4dd9b7e398eccd7286f5878b6a93c2204e9c80ad
Avogardo <avogardo0@gmail.com>
2019-04-15 14:50:37 +0200

    create form with references tests

--- angular-app/src/app/form/form-with-references/form-with-references.component.spec.ts
+++ angular-app/src/app/form/form-with-references/form-with-references.component.spec.ts
@@ -1,11 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import FormWithReferencesComponent from './form-with-references.component';
-import { FormsModule } from '@angular/forms';
+import FormModel from '../FormModel';
+import FormTypes from '../FormTypes';
 
 describe('FormWithReferencesComponent', () => {
   let component: FormWithReferencesComponent;
   let fixture: ComponentFixture<FormWithReferencesComponent>;
+  const formsWithReferences: FormModel[] = [
+    new FormModel(FormTypes.Text, 'text'),
+    new FormModel(FormTypes.Number, 1),
+    new FormModel(FormTypes.Submit, 'Submit'),
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,6 +29,7 @@ describe('FormWithReferencesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FormWithReferencesComponent);
     component = fixture.componentInstance;
+    component.inputs = formsWithReferences;
     fixture.detectChanges();
   });
 
@@ -33,5 +41,26 @@ describe('FormWithReferencesComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.form-wrapper')).toBeDefined();
+  });
+
+  it('always renders a form element', () => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('form')).toBeDefined();
+  });
+
+  it('always renders as much inputs ad form prop contain', () => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('input').length).toBe(formsWithReferences.length);
+  });
+
+  it('always renders inputs with type as in form models', () => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const inputs: HTMLInputElement[] = compiled.querySelectorAll('input');
+    inputs.forEach((input: HTMLInputElement, index: number) => {
+      expect(input.type).toBe(formsWithReferences[index].type);
+    });
   });
 });
