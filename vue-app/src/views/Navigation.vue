6c15f0fd345f59425ce17932716ac2684da16b10
Avogardo <avogardo0@gmail.com>
2019-04-16 09:01:45 +0200

    add navigation component

---
+++ vue-app/src/views/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <router-link to="/">Home</router-link>
+            </li>
+            <li>
+                <router-link to="/form-with-states">form with states</router-link>
+            </li>
+            <li>
+                <router-link to="/form-with-references">form with references</router-link>
+            </li>
+            <li>
+                <router-link to="/server-table">server table</router-link>
+            </li>
+            <li>
+                <router-link to="/big-table">big table</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Navigation',
+  }
+</script>
