192fce466b3429640e4ac2f33cc9e12c401c010f
Avogardo <avogardo0@gmail.com>
2019-04-20 00:03:41 +0200

    add table dom elements tests

--- vue-app/tests/unit/Table.spec.js
+++ vue-app/tests/unit/Table.spec.js
@@ -12,10 +12,39 @@ describe('Table.vue', () => {
     wrapper = shallowMount(Table, {
       localVue,
       router,
+      propsData: {
+        bigTable: true,
+      },
     });
   });
 
-  it('renders without crashing', () => {
+  it('renders without crashing with server data', () => {
+    const localVue = createLocalVue();
+    localVue.use(VueRouter);
+    const router = new VueRouter();
+    wrapper = shallowMount(Table, {
+      localVue,
+      router,
+      propsData: {
+        bigTable: false,
+      },
+    });
     expect(wrapper).toBeDefined();
+  });
+
+  it('renders without crashing with big static data', () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  it('always renders a table element', () => {
+    expect(wrapper.find('table').exists()).toBe(true);
+  });
+
+  it('always renders a table element', () => {
+    expect(wrapper.find('.table-wrapper').exists()).toBe(true);
+  });
+
+  it('always renders a 6 functional button elements', () => {
+    expect(wrapper.findAll('button')).toHaveLength(6);
   });
 });
